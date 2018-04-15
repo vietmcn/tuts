ca1dc6bdaef79b652a7c5ee77e3c51af2c82bd3a
Vietmcn <vietmcn.com@gmail.com>
2018-04-15 14:34:56 +0700

    update

--- Extend/Tooltip/class.ajax.php
+++ Extend/Tooltip/class.ajax.php
@@ -12,13 +12,13 @@ if ( !class_exists( 'ViExtend_Tooltip_ajax' ) ) {
             add_action( 'wp_enqueue_scripts', array( $this, 'ViExtend_tootlip_scripts' ) );
             add_action( 'wp_ajax_ViExtend_tootlip', array( $this, 'ViExtend_tootlip' ) ); // wp_ajax_{action}
             add_action( 'wp_ajax_nopriv_ViExtend_tootlip', array( $this, 'ViExtend_tootlip' ) ); // wp_ajax_nopriv_{action}
-            add_action( 'wp_footer', array( $this, 'print_script') ); // wp_ajax_nopriv_{action}
             add_action( 'woocommerce_before_shop_loop_item', array( $this, 'before_shop_loop_item' ) );
+            add_action( 'wp_head', array( $this, 'ViExtend_Style_print') );
         }
         public function ViExtend_tootlip_scripts() {
-            wp_enqueue_style( 'Jquery_UI_CSS', 'https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.css', '1.12.1', 'all' );
-            wp_enqueue_script( 'ViExtend_Tooltip', plugins_url( '/love.js', __FILE__ ), array('jquery'), '1.0', true );
+            //wp_enqueue_style( 'Jquery_UI_CSS', '//cdnjs.cloudflare.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.css', '1.12.1', 'all' );
             wp_enqueue_script( 'Jquery_UI', '//cdnjs.cloudflare.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js', array( 'jquery' ), '1.12.1', true );
+            wp_enqueue_script( 'ViExtend_Tooltip', get_template_directory_uri().'/tuts/Extend/Tooltip/tooltip.js', array( 'jquery' ), '0.1', true );
             $Query = new WP_Query( array(
                 'post_type' => 'product',
             ) );
@@ -28,7 +28,7 @@ if ( !class_exists( 'ViExtend_Tooltip_ajax' ) ) {
                     $id[] .= 'id:'.$Query->post->ID;
                 endwhile;
                 wp_localize_script( 'ViExtend_Tooltip', 'ViExtend_tootlip', array(
-                    'ajax_url' => admin_url( 'admin-ajax.php' ),
+                    'ajaxurl' => site_url() . '/wp-admin/admin-ajax.php',
                     'check_nonce' => wp_create_nonce('app-nonce'),
                 ) );
             } else {
@@ -38,22 +38,43 @@ if ( !class_exists( 'ViExtend_Tooltip_ajax' ) ) {
         public function before_shop_loop_item()
         {
             global $post;
-            echo '<a id="p_'.$post->ID.'" href="'.get_permalink().'">';;
+            echo '<a id="p_'.$post->ID.'" class="ViExtend-tooltip ViExtend-tooltips" href="'.get_permalink().'" data-tooltip="Đang tải..." title="'.get_the_title().'">';;
+        }
+        function get_price_stock( $att )
+        {
+            $price_regular = get_post_meta( $att['product_id'], '_regular_price', true );
+            $price_sale = get_post_meta( $att['product_id'], '_sale_price', true );
+            $sku = get_post_meta( $att['product_id'], '_sku', true );
+            $price_saleClass = ( $price_sale ) ? 'price_s' : '';
+
+            $out  = '<div class="price_all stock">';
+            $out .= '<div class="product_sku">Mã sản phẩm: <span>'.$sku.'</span></div>';
+            if ( $price_sale ) {
+                $out .= '<div class="price_ '.$price_saleClass.'">Giá: <del>'.number_format( $price_regular ).'<strong>đ</strong></del></div>';
+                $out .= '<div class="price_sales">Giá khuyến mãi: <ins class="price_sale">'.number_format( $price_sale ).'<strong>đ</strong></ins></div>';
+            } else {
+                $out .= '<div class="price_">Giá: <ins>'.number_format( $price_regular ).'<strong>đ</strong></ins></div>';
+            }
+            $out .= '</div>';
+            return $out;
         }
         public function ViExtend_tootlip_item( $att )
         {
-            $out  = '<div id="ViEXtend_tootltip">';
-            $out .= '<h4>'.get_the_title().'</h4>';
-            $out .= get_post_meta( $att['product_id'], '_regular_price', true );
-            $out .= '</div>';
-            echo $att['product_id'];
+            $out  = '<aside id="ViEXtend_tootltip">';
+            $out .= '<header>';
+            $out .= '<h4>'.get_the_title( $att['product_id'] ).'</h4>';
+            $out .= $this->get_price_stock( $att['product_id'] );
+            $out .= '</header>';
+            $out .= '<footer>';
+            $out .= '</footer>';
+            $out .= '</aside>';
+            echo $out;
         }
         public function ViExtend_tootlip()
         {
             global $App_getcontent;
             check_ajax_referer( 'app-nonce', 'security' );
             header("Content-Type: text/html");
-            #$args = json_decode( stripslashes( $_GET['userid'] ), true );
             // Get Tooltip Woocommerce
             $this->ViExtend_tootlip_item( array(
                 'product_id' => $_GET['userid']
@@ -61,52 +82,58 @@ if ( !class_exists( 'ViExtend_Tooltip_ajax' ) ) {
             //Ngắt Vòng Lặp
             die;
         }
-        public function print_script()
+        public function ViExtend_Style_print()
         {
             ?>
-            <script>
-            jQuery(document).ready(function ($) {
-            $(document).ready(function(){
-
-// initialize tooltip
-$( ".product a" ).tooltip({
-  track:true,
-  open: function( event, ui ) {
-  var id = this.id;
-  var split_id = id.split('_');
-  var userid = split_id[1];
-  var data = {
-			'action': 'ViExtend_tootlip',
-            'security' : ViExtend_tootlip.check_nonce,
-            'userid': userid
-	};
-  $.ajax({
-   url: ViExtend_tootlip.ajaxurl,
-   type: 'GET',
-   data: data,
-   cache : false,
-   async: true,
-   success: function( response ){
-    $('.product').html( response );
-  },
-  complete: function( response ) {
-        // $('.product').hide(); // This is called when the success part is still executing
-  }
- });
- }
-});
-
-$(".product a").mouseout(function(){
-  // re-initializing tooltip
-  $(this).attr('title','Please wait...');
-  $(this).tooltip();
-  $('.ui-tooltip').hide();
-});
-
-});
-});
-</script>
-            <?php
+            <style>
+            #ViEXtend_tootltip h4 {
+                background-color: red;
+                color: #fff;
+                padding: 10px;
+                font-weight: normal;
+                font-size: 16px;
+                text-transform: uppercase;
+            }
+            .ui-tooltip {
+                padding: 0px;
+                border: 0px;
+                width: 250px;
+                background-color: #fff;
+                font-size: 14px;
+                border: 1px solid #ededed;
+            }
+            .price_all {
+                padding : 10px;
+            }
+            #ViEXtend_tootltip header {
+                margin-bottom: 2rem;
+                border-bottom: 1px solid rgba(220, 220, 220, 0.5215686274509804);
+            }
+            .price_all .product_sku {
+                margin-bottom: 10px;
+            }
+            .price_all .price_sales {
+                margin-top: 10px;
+            }
+            .price_all .price_sale, .price_all ins {
+                font-size: 17px;
+                font-weight: 700;
+                color: red;
+            }
+            .price_all ins {
+                background: none;
+            } 
+            .price_all ins strong {
+                font-weight: 700;
+            }
+            .price_all strong {
+                font-weight: normal;
+            }
+            body .ui-visual-focus {
+                box-shadow: none;
+            }
+            </style>
+            <?php 
         }
     }
     
---
+++ Extend/Tooltip/tooltip.js
@@ -0,0 +1,38 @@
+jQuery(document).ready(function ($) {
+    // initialize tooltip
+    $( ".product a.ViExtend-tooltip" ).tooltip({
+      track:true,
+      items: "[data-tooltip]",
+      content: function() {
+        var element = $( this );
+        if ( element.is( "[data-tooltip]" ) ) {
+            return element.attr( "data-tooltip" );
+        }
+      },
+      open: function( event, ui ) {
+        var id = this.id;
+        var split_id = id.split('_');
+        var userid = split_id[1];
+        var data = {
+                'action': 'ViExtend_tootlip',
+                'security' : ViExtend_tootlip.check_nonce,
+                'userid': userid
+            };
+        $.ajax({
+            url: ViExtend_tootlip.ajaxurl,
+            type: 'GET',
+            data: data,
+            async: true,
+            success: function( data ) {
+                $("#"+id).tooltip('option','content', data);
+            },
+        });
+    }
+    });
+    
+    $(".product a.ViExtend-tooltips").mouseout(function(){
+        // re-initializing tooltip
+        $(this).tooltip();
+        $('.ui-tooltip').hide();
+    });
+});
